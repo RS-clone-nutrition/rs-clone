0cc939d5ec69940c78e80c4ec6e854cb34c171c5
Lojkuc <lojkucvladislav@gmail.com>
2023-02-02 21:52:59 +0300

    feat:add about page

--- client/src/pages/logUp.ts
+++ client/src/pages/logUp.ts
@@ -7,18 +7,21 @@ class LogUp {
 
   render() {
     this.main.innerHTML = `
-    <div class="form">
-      <div class="form__block">
-        <h1>Create Account</h1>
-        <h2>Create account using email</h2>
-          <div class="form__inputs">
-            <input type="email" placeholder="Email Address">
-            <input type="password" placeholder="Password">
-            <button>Register</button>
-          </div>
-        <span>Got an account?<a href="/signup"> Sign In</a></span>
+    <div class="container">
+      <div class="form">
+        <div class="form__block">
+          <h1>Create Account</h1>
+          <h2>Create account using email</h2>
+            <div class="form__inputs">
+              <input type="email" placeholder="Email Address">
+              <input type="password" placeholder="Password">
+              <button>Register</button>
+            </div>
+          <span>Got an account?<a href="/signup"> Sign In</a></span>
+        </div>
       </div>
     </div>
+
     `;
   }
 }
--- client/src/pages/signIn.ts
+++ client/src/pages/signIn.ts
@@ -7,20 +7,23 @@ class SignIn {
 
   render() {
     this.main.innerHTML = `
-    <div class="form">
-    <div class="form__block">
-      <h1>Sign In</h1>
-      <h2>Sign in with your FatSecret account</h2>
-      <div class="form__inputs">
-        <input type="email" placeholder="Email Address">
-        <div class="form__submit">
-          <input type="password" placeholder="Password">
-          <button>Sign Up</button>
+    <div class="container">
+      <div class="form">
+        <div class="form__block">
+          <h1>Sign In</h1>
+          <h2>Sign in with your FatSecret account</h2>
+          <div class="form__inputs">
+            <input type="email" placeholder="Email Address">
+            <div class="form__submit">
+              <input type="password" placeholder="Password">
+              <button>Sign Up</button>
+            </div>
+          </div>
+          <span>Become a member! <a href="/login">Create Account</a></span>
         </div>
       </div>
-      <span>Become a member! <a href="/login">Create Account</a></span>
     </div>
-  </div>
+
     `;
   }
 }
