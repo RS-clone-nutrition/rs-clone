aa3ed0ca8ca69248e943fcbcf8f78d9d080f792f
Lojkuc <lojkucvladislav@gmail.com>
2023-02-03 20:14:17 +0300

    feat:add icons

--- client/src/pages/myFatSecret.ts
+++ client/src/pages/myFatSecret.ts
@@ -6,7 +6,26 @@ class MyFatSecret {
   }
 
   render() {
-    this.main.innerHTML = '<h1>My FatSecret content</h1>';
+    this.main.innerHTML = `
+    <div class="container">
+    <div class="myfatsecret">
+      <div class="myfatsecret__tabs">
+        <button class="myfatsecret__card">
+          <img src="../src/img/calender-icon.svg" alt="calendar">
+          <span class="myfatsecret__info-calendar">WED 01</span>
+        </button>
+        <button class="myfatsecret__card">
+          <img src="../src/img/food-icon.svg" alt="food">
+          <span class="myfatsecret__info-food">0 kcal</span>
+        </button>
+        <button class="myfatsecret__card">
+          <img src="../src/img/fitness-icon.svg" alt="fitness">
+          <span class="myfatsecret__info-fitness">0 kcal</span>
+        </button>
+      </div>
+    </div>
+  </div>
+    `;
   }
 }
 
