b3e774910ace5818035618fda3943d094ec180e4
Evgen <yevgenchikk@mail.ru>
2023-02-17 11:54:18 +0300

    feat: add model for database

---
+++ server/models/User.js
@@ -0,0 +1,13 @@
+import { Schema, model } from "mongoose";
+
+const User = new Schema({
+  username: { type: String, unique: true, require: true },
+  password: { type: String, require: true },
+  weight: { type: String, require: true },
+  goal: { type: String, require: true },
+  height: { type: String, require: true },
+  age: { type: String, require: true },
+  gender: { type: String, require: true },
+})
+
+export default model('User', User);
