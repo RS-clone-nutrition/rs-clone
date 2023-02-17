905bec2ad94802f2eba88c9bc3c9988408e0beb1
Evgen <yevgenchikk@mail.ru>
2023-02-17 11:58:23 +0300

    feat: add express check

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,8 +1,17 @@
 import Router from "express";
+import { check } from "express-validator";
 
 const router = new Router();
 
-router.post('/login')
+router.post('/login',
+  [
+    check('username', "username can't be empty").notEmpty(),
+    check('weight', "weight can't be empty").notEmpty(),
+    check('height', "height can't be empty").notEmpty(),
+    check('age', "age can't be empty").notEmpty(),
+    check('gender', "gender can't be empty").notEmpty(),
+    check('password', "password must be longer than 4 and shorter than 10 characters").isLength({ min: 4, max: 10 }),
+  ],)
 router.post('/signup')
 router.get('/users')
 
