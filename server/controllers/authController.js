315845afb4743e3ff66b67ea1bd04d99a8866075
Evgen <yevgenchikk@mail.ru>
2023-02-17 12:04:02 +0300

    feat: add errors and password hash

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,8 +6,20 @@ import { validationResult } from "express-validator";
 class authController {
   async registration(req, res) {
     try {
-      const { username, password, weight, height, age, gender } = req.body
+      const errors = validationResult(req);
 
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: 'Registration error:' + ' ' + errors.array()[0].msg + '\n Please fill in all fields' })
+      }
+
+      const { username, password, weight, height, age, gender } = req.body
+      const candidate = await User.findOne({ username })
+
+      if (candidate) {
+        return res.status(400).json({ message: 'user already exists' })
+      }
+
+      const hashPassword = bcrypt.hashSync(password, 3);
       const user = new User({ username: username, password: hashPassword, weight: weight, goal: goal, height: height, age: age, gender: gender })
 
       await user.save()
