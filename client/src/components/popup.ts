7fb6e53d90562f7373db76a812e46b128a9fb12b
Evgen <yevgenchikk@mail.ru>
2023-02-25 19:25:09 +0300

    fix: regex

--- client/src/components/popup.ts
+++ client/src/components/popup.ts
@@ -94,7 +94,7 @@ const popup = {
     block.innerHTML += `
     <div class = "popup__products-table__add-block__add-item">
       <span class="popup__products-table__food">
-      <input type="checkbox" class="checkbox-addItem" id="${!Number.isNaN(+uri) ? uri : uri.match(/(?<=#).*/gi)}">
+      <input type="checkbox" class="checkbox-addItem">
       ${label}
       </span>
       <span class="popup__products-table__amount">
